f16a2ad3e06e946d59855d0744f7ab1940d9b155
Richard Ramos <info@richardramos.me>
2019-09-04 15:04:57 -0400

    fix: react - pt2.

--- examples/react/src/MyComponentObserver.js
+++ examples/react/src/MyComponentObserver.js
@@ -1,17 +1,20 @@
 import React from "react";
-import {observe} from "phoenix/react";
+import { observe } from "phoenix/react";
 
-const MyComponent = props => {
-  const { escrow, myCustomProperty } = props;
-  if(!escrow) return <p>Loading...</p>;
-  return (
-    <ul>
-      <li>
-        {escrow.buyer} {escrow.seller} - <b>EscrowID:{escrow.escrowId}</b>{" "}
-        <small>{myCustomProperty}</small>
-      </li>
-    </ul>
-  );
+const MyComponent = ({ eventData }) => {
+  // Handle initial state when no data is available
+  if (!eventData) {
+    return <p>No data</p>;
+  }
+
+  console.log("Data received", eventData);
+
+  return <ul>
+    <li><b>someValue: </b> {eventData.someValue}</li>
+    <li><b>anotherValue: </b> {eventData.anotherValue}</li>
+  </ul>;
 };
 
+// MyComponent will now observe any observable prop it receives
+// and update its state whenever the observable emits an event
 export default observe(MyComponent);
