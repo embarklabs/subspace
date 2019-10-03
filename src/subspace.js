85d8d80db89523a4bf9933a9d772d31a4a3d5c6e
Iuri Matias <iuri.matias@gmail.com>
2019-10-03 11:21:05 -0400

    implement map method for trackProperty and trackEvent

--- src/subspace.js
+++ src/subspace.js
@@ -1,5 +1,5 @@
 import { ReplaySubject } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 import equal from 'fast-deep-equal';
 import Database  from './database.js';
 import Events from 'events';
@@ -51,7 +51,24 @@ export default class Subspace {
 
   // TODO: get contract abi/address instead
   trackEvent(contractInstance, eventName, filterConditionsOrCb) {
-    return this.eventSyncer.track(contractInstance, eventName, filterConditionsOrCb, this.latestBlockNumber - this.options.refreshLastNBlocks);
+    let returnSub = this.eventSyncer.track(contractInstance, eventName, filterConditionsOrCb, this.latestBlockNumber - this.options.refreshLastNBlocks);
+
+    returnSub.map = (prop) => {
+      return returnSub.pipe(map((x) => {
+        if (typeof(prop) === "string") {
+          return x[prop];
+        }
+        if (Array.isArray(prop)) {
+          let newValues = {}
+          prop.forEach((p) => {
+            newValues[p] = x[p]
+          })
+          return newValues
+        }
+      }))
+    }
+
+    return returnSub;
   }
 
   clearDB(collection) {
@@ -114,7 +131,24 @@ export default class Subspace {
 
     this.callables.push(callContractMethod);
 
-    return sub.pipe(distinctUntilChanged((a, b) => equal(a, b)));
+    let returnSub = sub.pipe(distinctUntilChanged((a, b) => equal(a, b)));
+
+    returnSub.map = (prop) => {
+      return returnSub.pipe(map((x) => {
+        if (typeof(prop) === "string") {
+          return x[prop];
+        }
+        if (Array.isArray(prop)) {
+          let newValues = {}
+          prop.forEach((p) => {
+            newValues[p] = x[p]
+          })
+          return newValues
+        }
+      }))
+    }
+
+    return returnSub;
   }
 
   trackBalance(address, erc20Address) {
