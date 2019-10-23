cf241ed2dee4a03c699cc06ac4c74fdeb27ec486
Richard Ramos <info@richardramos.me>
2019-10-23 14:44:45 -0400

    feat: vue example

---
+++ examples/vue/src/components/MyComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul v-if="!!eventData$">
+    <li><b>someValue: </b> {{eventData$.someValue}}</li>
+    <li><b>anotherValue: </b> {{eventData$.anotherValue}}</li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  name: 'MyComponent',
+  props: {
+    eventData: Object
+  },
+  subscriptions() {
+    return {
+      eventData$: this.eventData
+    }
+  }
+}
+</script>
