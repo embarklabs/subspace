352c6d69f3e8fc10925049a24d92036432638449
Richard Ramos <info@richardramos.me>
2019-10-24 11:33:02 -0400

    feat: adding svelte dapp

---
+++ examples/svelte/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+// import web3 from './web3';
+// import MyContract from './MyContract';
+import { onMount } from 'svelte';
+import Subspace from "@status-im/subspace";
+
+let MyContractInstance;
+let subspace;
+let myEventObservable$;
+
+onMount(async () => {
+  //MyContractInstance = await MyContract.getInstance(); 
+
+ // subspace = new Subspace(web3.currentProvider);
+  // await subspace.init();
+
+  //myEventObservable$ = subspace.trackEvent(MyContractInstance, "MyEvent", {filter: {}, fromBlock: 1 });
+});
+
+
+let name = 'world';
+	
+</script>
+
+<h1>Hello {name}!</h1>
