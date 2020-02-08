f4264f7b761cef03def534b80917fbd32b8115a6
Richard Ramos <info@richardramos.me>
2020-02-07 21:41:00 -0400

    feat: block stats

---
+++ test/test8.js
@@ -0,0 +1,16 @@
+const Web3Eth = require('web3-eth');
+const Subspace = require('../dist/index.js').default;
+
+let eth = new Web3Eth("https://mainnet.infura.io/v3/562ba55287324547adbdd59b1dabc869");
+
+async function run() {
+  const subspace = new Subspace(eth.currentProvider);
+  await subspace.init()
+
+  subspace.trackBlockNumber().subscribe((v) => console.log("Blocknumber", v));
+  subspace.trackGasPrice().subscribe((v) => console.log("GasPrice", v));
+  subspace.trackBlock().subscribe((v) => console.log("Block.difficulty", v.difficulty));
+  subspace.trackAverageBlocktime().subscribe((v) => console.log("Blocktime", v));
+}
+
+run()
